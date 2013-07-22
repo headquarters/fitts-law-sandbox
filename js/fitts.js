36f7ab351619c21e03ea184bd6a665214da0519c
Michael Head <michaelehead@gmail.com>
2013-07-22 16:45:36 -0400

    Reorganized code for better project layout. Refactored JS to use simple module pattern.

---
+++ js/fitts.js
@@ -0,0 +1,61 @@
+var Fitts = function(){
+	var self = {};
+	
+	//"back of the envelope" calculations in milliseconds (Raskin, 2000)
+	var a = 50;
+	var b = 150;
+	
+	/**
+	 * Calculates Fitts's Law to return average Movement Time.
+	 */
+	self.getMovementTime = function(distanceFromMouseToTarget, targetWidth){
+		return (a + b * (Math.log( (distanceFromMouseToTarget / targetWidth) + 1) / Math.log(2) ));
+	};
+	
+	return self;
+}();
+
+var Sandbox = function(){
+	var self = {};
+	var target = document.getElementById('target');
+	var sandbox = document.getElementById('sandbox');
+	var result = document.getElementById('result');
+	
+	self.init = function(){
+		sandbox.addEventListener('mousemove', self.calculateMovementTime, true);	
+	};
+	
+	//sandbox: offsetLeft is 30, offsetTop is 80
+	self.calculateMovementTime = function(e){
+		var targetWidth = target.clientWidth;
+		//compensate for #sandbox offset
+		//TODO: calculate from appropriate corner, rather than always top left, depending on where mouse is
+		var targetX = target.offsetLeft - sandbox.offsetLeft;
+		var targetY = target.offsetTop - sandbox.offsetTop;
+		var mouseX = e.pageX - sandbox.offsetLeft;
+		var mouseY = e.pageY - sandbox.offsetTop;
+		
+		//use e.offsetX and e.offsetY to get mouse relative to #sandbox
+		var distanceFromMouseToTarget = self.getDistance(targetX, targetY, mouseX, mouseY);
+		
+		console.log(distanceFromMouseToTarget);
+		
+		var movementTime = Fitts.getMovementTime(distanceFromMouseToTarget, targetWidth);
+		//console.log(movementTime);
+		result.innerHTML = movementTime;
+	};
+
+	self.getDistance = function(x1, y1, x2, y2){
+		return Math.sqrt( Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2) );
+	};
+	
+	return self;
+}();
+
+Sandbox.init();
+
+
+
+
+
+	
